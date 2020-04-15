68af0401e7cbb38a3e27c8051872efc118624bda
a <aymeric.touche@gmail.com>
2020-04-15 02:28:23 +0200

    maj aboutUS

---
+++ app/Controller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+use \Psr\Http\Message\RequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+class Controller{
+
+    private $container;
+
+    public function __construct($container){
+        $this->container = $container;
+    }
+
+    public function map(Request $request, Response $response){
+        $this->container->view->render($response,'map.twig');
+    }
+
+    public function aboutUs(Request $request, Response $response){
+        $this->container->view->render($response,'aboutUs.twig');
+    }
+    public function analyse(Request $request, Response $response){
+        $this->container->view->render($response,'analyse.twig');
+    }
+}
